7a86ee7266837faaee3e5e55972327d28ae73fd7
Deciptikon <deciptikon@mail.ru>
2024-12-10 04:43:45 +0300

    border

--- script.js
+++ script.js
@@ -20,3 +20,22 @@ ctx.font = "100px Arial";
 ctx.textAlign = "center";
 ctx.textBaseline = "middle";
 ctx.fillText("Hello Canvas!", canvas.width / 2, canvas.height / 2);
+
+drawRoundedRect(ctx, [canvas.width, canvas.height], 30, 30);
+
+function drawRoundedRect(ctx, canvSize, radius, strokeWidth) {
+  let x = 0;
+  let y = 0;
+  [width, height] = canvSize;
+  let rad = radius + strokeWidth;
+
+  ctx.beginPath();
+  ctx.lineWidth = 2 * strokeWidth;
+  ctx.strokeStyle = "white";
+  ctx.moveTo(x + rad, y);
+  ctx.arcTo(x + width, y, x + width, y + height, rad); // Верхний правый угол
+  ctx.arcTo(x + width, y + height, x, y + height, rad); // Нижний правый угол
+  ctx.arcTo(x, y + height, x, y, rad); // Нижний левый угол
+  ctx.arcTo(x, y, x + width, y, rad); // Верхний левый угол
+  ctx.stroke();
+}

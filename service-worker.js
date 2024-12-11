7e27eecf1198f9f1d3ca3d4a1d672d0c10cc5e02
Deciptikon <deciptikon@mail.ru>
2024-12-11 05:31:48 +0300

    ...

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,12 @@
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open("v1").then((cache) => {
-      return cache.addAll(["/", "/styles.css", "/script.js"]);
+      return cache.addAll([
+        "/",
+        "/styles.css",
+        "/script.js",
+        "/images/favicon.ico",
+      ]);
     })
   );
 });
